6c68973ce7e4f25c6470951ffb90a194d41c9d23
seki <102042736+HERUESTA@users.noreply.github.com>
2025-06-02 22:40:40 +0900

    データベースの設定の修正

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ module App
     config.load_defaults 8.0
     config.autoload_lib(ignore: %w[assets tasks])
     config.api_only = true
-    config.time_zone = 'Tokyo'
+    config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
   end
